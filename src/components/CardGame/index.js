eb816898c5edeb7c062f76f4259261c1ecc82063
Jeancarlos-Si <jeancarlos.si@outlook.com>
2022-01-23 15:58:19 -0300

    Criação deCriação deCriação deObject creation for the Board Game returning a </section> containing a template string that has the variable

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -3,9 +3,9 @@ import "./style.css"
 function CardGame() {
     return `
       <article class="card-game">
-        <img src = "./images/alura-pixel.png" alt="Logo da Alura"
-      </article>
+        <img src = "./images/alura-pixel.png" alt="Logo da Alura">
+      </article> 
     `;
   }
-  
+
   export default CardGame;
---
+++ src/components/PlayerScore/index.js
@@ -0,0 +1,11 @@
+function PlayerScore(player) {
+    return `
+        <div class="player-score">
+            <a>Player:${player}</a>
+        </div>
+    `;
+}   
+
+PlayerScore("Jean")
+
+  export default PlayerScore;
