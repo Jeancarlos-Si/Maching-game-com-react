cb110881029221a5adb25b81973c29c10267ba13
Jeancarlos-Si <jeancarlos.si@outlook.com>
2022-01-24 12:39:37 -0300

    Criação de CardFrontBack e alteração do main para exibir frente e verso da costa

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 import "./src/style/setttings/color.css"
-
 import "./src/style/generic/reset.css"
 import "./src/style/elements/base.css"
+
 import BoardGame from "./src/objects/BoardGame";
 import ScoreBoard from "./src/objects/ScoreBoard";
-
-
-
-
 
 const $root = document.querySelector("#root");
 
