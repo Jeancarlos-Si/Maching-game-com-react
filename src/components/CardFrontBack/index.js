94fdf64654fa553f697b0597619daf252c7aaf4a
Jeancarlos-Si <jeancarlos.si@outlook.com>
2022-01-24 16:34:06 -0300

    Adicionando imagem ao BackCard

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,12 +1,12 @@
 import CardGame from "../CardGame";
 
-function CardFrontBack () {
-    return /*html*/`
+function CardFrontBack() {
+  return /*html*/ `
     <article class="card-front-back">
-        ${CardGame()}
-        ${CardGame()}
+      ${CardGame()}
+      ${CardGame("javascript", "Logo do JavaScript")}
     </article>
-    `
+  `;
 }
 
-export default CardFrontBack
+export default CardFrontBack;
--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,11 +1,13 @@
-import "./style.css"
+import "./style.css";
 
-function CardGame() {
-    return `
-      <article class="card-game">
-        <img src = "./images/alura-pixel.png" alt="Logo da Alura">
-      </article>
+function CardGame(icon="alura-pixel",alt="Logo da Alura") {
+    return /*html*/`
+    <article class="card-game">
+    <img src="images/${icon}.png" alt="${alt}">
+  </article>
     `;
   }
   
   export default CardGame;
+
+  
