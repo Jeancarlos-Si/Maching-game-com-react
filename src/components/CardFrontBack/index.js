259b204112e50e68f9d117ed4e61b7f4d103ddd0
Jeancarlos-Si <jeancarlos.si@outlook.com>
2022-01-25 13:23:55 -0300

    Add onclick function javaScript

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -2,8 +2,25 @@ import CardGame from "../CardGame";
 import "./style.css";
 
 function CardFrontBack() {
+  window.cardFrontBack = {} //Criação de escopo global "Local" para ser utilizado apenas em CardFrontBack
+  window.cardFrontBack.handleClick = (event) => {
+    const $origin = event.target;
+    const $cardFrontBack = $origin.closest('.card-front-back')
+
+
+    $cardFrontBack.classList.toggle('-active')
+  }
+  //Faz a mesma coisa que $cardFrontBack.classList.toggle('-active')
+  //if($cardFrontBack.classList.contains('-active')){
+  //  $cardFrontBack.classList.remove('-active')
+  // }else{
+  //  $cardFrontBack.classList.add('-active')  
+  //}
+  
+    
+
   return /*html*/ `
-    <article class="card-front-back">
+    <article class="card-front-back" onclick="cardFrontBack.handleClick(event)">
       <div class="card -front">
         ${CardGame()}
       </div>
