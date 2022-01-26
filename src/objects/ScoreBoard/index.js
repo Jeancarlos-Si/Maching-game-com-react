24799ffe37bc49ece89315a2fc100f01102ee60f
Jeancarlos-Si <jeancarlos.si@outlook.com>
2022-01-26 17:18:40 -0300

    Add component versus

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,11 +1,13 @@
 
 import PlayerName from "../../components/PlayerName";
+import VsPlayer from "../../components/versus";
 import "./style.css";
 
 function ScoreBoard() {
     return  /*html*/`
     <header class="score-board">
         ${PlayerName('João')}
+        ${VsPlayer()}
         ${PlayerName('Pedro')}
     </header>
     `
