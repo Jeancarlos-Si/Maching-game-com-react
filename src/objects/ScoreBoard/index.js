0f46b7372586a591a8c97a05d991ce3d4e3baf96
Jeancarlos-Si <jeancarlos.si@outlook.com>
2022-01-27 08:46:13 -0300

    Create styles fo the PlayerScore component

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,13 +1,16 @@
 
 import PlayerName from "../../components/PlayerName";
 import VsPlayer from "../../components/VsPlayer";
+import PlayerScore from "../../components/PlayerScore";
 import "./style.css";
 
 function ScoreBoard() {
     return  /*html*/`
     <header class="score-board">
         ${PlayerName('João')}
+        ${PlayerScore(2)}
         ${VsPlayer()}
+        ${PlayerScore(2)}
         ${PlayerName('Pedro')}
     </header>
     `
